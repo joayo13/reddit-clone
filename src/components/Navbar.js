13b1b3ab40fb96d449d084d80d02ec538b1306e5
Jordan Ayotte <jordanayotte13@outlook.com>
2022-07-29 16:42:03 -0400

    added better code comments for navbar section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,10 +13,11 @@ const Navbar = () => {
         <img src={redditIcon} className='w-10'></img>
         <h1 className='text-xl md:text-1xl text-gray-700'>!Reddit</h1>
         </div>
-        {/* links on desktop */}
+        {/* sign up and login buttons */}
         <ul className='flex text-1xl ml-auto gap-6'>
           <button class='hidden md:block w-32 py-1 border-blue-500 font-bold border rounded-full text-blue-500 hover:bg-slate-100 text-sm'>Log In</button>
           <button class='hidden md:block w-32 py-1 bg-blue-500 font-bold border rounded-full text-white hover:bg-blue-400 text-sm'>Sign Up</button>
+          {/* dropdown menu button */}
           <button class='flex justify-center md:border-slate-200 w-20 py-1 border-slate-100 border' onClick={() => setMobileNavLinksVisible(!mobileNavLinksVisible)}>
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
@@ -26,10 +27,8 @@ const Navbar = () => {
             </svg>
         </button>
         </ul>
-        {/* hamburger menu for mobile */}
-        
-        {/* dropdown links on mobile */}
       </header>
+      {/* dropdown menu contents */}
       { mobileNavLinksVisible ? <ul className='mobileNav flex flex-col justify-evenly absolute right-4 overflow-hidden bg-white rounded-sm z-10 border border-slate-200 text-sm font-semibold'>
         <li className='flex gap-2 items-center cursor-pointer px-2 py-4 hover:bg-slate-200'>
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
@@ -58,7 +57,7 @@ const Navbar = () => {
           Sign Up or Log In
         </li>
       </ul> : null}
-      {/* dark overlay for page outside of dropdown */}
+      {/* full screen button for disabling dropdown if clicking outside of it */}
       { mobileNavLinksVisible ? <button onClick={() => setMobileNavLinksVisible(!mobileNavLinksVisible)} className='fixed top-0 right-0 bottom-0 left-0 h-full w-full cursor-default'></button> : null}
     </div>
   )

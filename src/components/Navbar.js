2ffdda254fc1da9640d800f2f94ba539d044d9c8
Jordan Ayotte <jordanayotte13@outlook.com>
2022-07-29 16:36:50 -0400

    added icons to navbar dropdown menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,11 +30,33 @@ const Navbar = () => {
         
         {/* dropdown links on mobile */}
       </header>
-      { mobileNavLinksVisible ? <ul className='mobileNav flex flex-col justify-evenly absolute right-4 overflow-hidden bg-white rounded-sm z-10 border border-slate-200 text-sm font-bold'>
-        <li className='cursor-pointer px-8 py-4 hover:bg-slate-200'>Settings</li>
-        <li className='cursor-pointer px-8 py-4 hover:bg-slate-200'>Help Center</li> 
-        <li className='cursor-pointer px-8 py-4 hover:bg-slate-200'>{`Terms & Policies`}</li>
-        <li className='cursor-pointer px-8 py-4 hover:bg-slate-200'>FAQ's</li>
+      { mobileNavLinksVisible ? <ul className='mobileNav flex flex-col justify-evenly absolute right-4 overflow-hidden bg-white rounded-sm z-10 border border-slate-200 text-sm font-semibold'>
+        <li className='flex gap-2 items-center cursor-pointer px-2 py-4 hover:bg-slate-200'>
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+          <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+        </svg>
+          Settings
+        </li>
+        <li className='flex gap-2 items-center cursor-pointer px-2 py-4 hover:bg-slate-200'>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+          Help Center
+        </li> 
+        <li className='flex gap-2 items-center cursor-pointer px-2 py-4 hover:bg-slate-200'>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
+          </svg>
+          {`Terms & Policies`}
+        </li>
+        <div className='w-auto h-px bg-slate-200'></div>
+        <li className='flex gap-2 items-center cursor-pointer px-2 py-4 hover:bg-slate-200'>
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+          Sign Up or Log In
+        </li>
       </ul> : null}
       {/* dark overlay for page outside of dropdown */}
       { mobileNavLinksVisible ? <button onClick={() => setMobileNavLinksVisible(!mobileNavLinksVisible)} className='fixed top-0 right-0 bottom-0 left-0 h-full w-full cursor-default'></button> : null}

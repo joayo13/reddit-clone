cd9b310b2577c4d7983fa2e9da8d7de8e80679df
Jordan Ayotte <jordanayotte13@outlook.com>
2022-07-30 23:29:26 -0400

    added rough beginnings of login component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
+import LogIn from "./LogIn";
 import Navbar from "./Navbar";
 
 
 function App() {
+  const [logInPopUp, setLogInPopUp] = useState(false)
+  const [signUpPopUp, setSignUpPopUp] = useState(false)
   return (
     <>
     <Navbar/>
+    <LogIn/>
     </>
   );
 }
---
+++ src/components/LogIn.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LogIn() {
+  return (
+      <div className='flex w-screen h-screen justify-center items-center top-0'>
+      <div className='fixed w-screen h-screen left-0 top-0 right-0 bottom-0 bg-black opacity-50'></div>
+      <div className='fixed w-screen h-screen md:absolute md:w-1/3 md:h-2/3 bg-red-400'></div>
+    </div>
+  )
+}
+
+export default LogIn

1fe29063b557941625d12f4bbb24148095072bbd
Jordan Ayotte <jordanayotte13@outlook.com>
2022-08-03 17:20:10 -0400

    added styling to nabar dropdown component, now shows users name too

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -71,9 +71,10 @@ const Navbar = (props) => {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
             </svg>
           </button> : 
-          <button className='flex justify-center py-1' onClick={() => setMobileNavLinksVisible(!mobileNavLinksVisible)}>
-            <img className='w-6' src={userInfo.profilePicture}></img>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 opacity-50" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+          <button className='flex relative py-1 px-2 gap-2 justify-start md:w-48 border border-white hover:border-slate-200 rounded-md' onClick={() => setMobileNavLinksVisible(!mobileNavLinksVisible)}>
+            <img className='w-6 hidden md:block rounded-full' src={userInfo.profilePicture}></img>
+            <h3 className='hidden md:block text-xs font-semibold'>{userInfo.username}</h3>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 opacity-50 md:absolute md:right-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
             </svg>
           </button>} 

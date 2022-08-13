7a7dc2a34ae287ed8c91a0842f737b593cbd52f1
Jordan Ayotte <jordanayotte13@outlook.com>
2022-08-13 18:44:36 -0400

    added proper dark mode styles for CreateUser component

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -38,9 +38,9 @@ function CreateUser(props) {
   return (
     <div className='fixed flex w-screen h-screen justify-center items-center top-0 overflow-y-hidden z-10'>
       <div className='fixed w-screen h-screen left-0 top-0 right-0 bottom-0 bg-black opacity-50'></div>
-      <div className='relative w-screen h-screen md:absolute md:w-[50rem] md:h-[40rem] bg-white dark:bg-gray-900 dark:text-white rounded-md shadow-xl overflow-hidden'>
-        <img src={sideImage} className='hidden md:block absolute top-0 left-0 h-full' alt='planet'></img>
-        <div className='relative md:left-28 md:w-[43rem] w-full h-full bg-white py-16 px-8 overflow-y-scroll'>
+      <div className='relative w-screen h-screen md:absolute md:w-[50rem] md:h-[40rem] dark:text-white rounded-md shadow-xl overflow-hidden'>
+        <img src={sideImage} className='hidden md:block absolute top-0 left-0 h-full invert' alt='planet'></img>
+        <div className='relative md:left-28 md:w-[43rem] w-full h-full dark:bg-gray-900 bg-white py-16 px-8 overflow-y-scroll'>
         <svg onClick={()=> props.setSignUpPopUp(false)} xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 absolute right-2 top-2 opacity-50" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
         </svg>
@@ -52,7 +52,7 @@ function CreateUser(props) {
         {error && <div className='w-full bg-red-400 p-4 font-semibold rounded-md mt-4'>{error}</div>}
         <form onSubmit={handleSubmit} className='mt-4 flex flex-col gap-6'>
         <p className='text-xs mt-2 md:w-1/3 text-center md:text-start'>Create a Username</p>
-        <input maxLength={15} minLength={4} placeholder='USERNAME' type='text' ref={usernameRef} className='bg-slate-100 py-4 indent-4 w-80 focus:outline-none'></input>
+        <input maxLength={15} minLength={4} placeholder='USERNAME' type='text' ref={usernameRef} className='bg-gray-100 dark:bg-gray-800 py-4 indent-4 w-80 focus:outline-none'></input>
           <button disabled={loading} type='submit' className=' bg-blue-500 py-2 rounded-full text-white w-80 font-semibold mx-auto md:mx-0 hover:bg-blue-400'>Create Account</button>
         </form>
         </div>

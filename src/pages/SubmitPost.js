bbd8ad4c7db9a900753bb1e2fb0fb1d912b510a1
Jordan Ayotte <jordanayotte13@outlook.com>
2022-10-18 18:24:51 -0400

    can now upload images to firebase

--- src/pages/SubmitPost.js
+++ src/pages/SubmitPost.js
@@ -2,12 +2,16 @@ import React, { useEffect, useState, useRef } from 'react'
 import { useAuth } from '../contexts/AuthContext'
 import { useParams, useNavigate} from 'react-router-dom'
 import { createPost, fetchSubredditData } from '../helpers/getSubredditDataFunctions';
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+
+
 
 
 function SubmitPost() {
     const { userInfo } = useAuth()
     const [subredditMetaData, setSubredditMetaData] = useState({})
     const [loading, setLoading] = useState(true)
+    const [fileSelected, setFileSelected] = useState(null)
     let { id } = useParams()
     const postTitleRef = useRef()
     const postTextRef = useRef()
@@ -15,6 +19,15 @@ function SubmitPost() {
     useEffect(() => {
         fetchSubredditData(setSubredditMetaData, setLoading, id)
     },[id])
+    const storage = getStorage();
+    const storageRef = ref(storage, `images/${fileSelected?.name}`);
+    async function upload() {
+        uploadBytes(storageRef, fileSelected).then((snapshot) => {
+            console.log('Uploaded a blob or file!');
+            console.log(snapshot)
+        });
+    }
+    
   return (
     <>{loading ? null :
         <div className='bg-gray-100 dark:bg-black min-h-screen'>
@@ -32,8 +45,9 @@ function SubmitPost() {
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                             </svg>
 
-                            <input type="file" className='hidden'></input>
+                            <input type="file" className='hidden' onChange={(file) => setFileSelected(file.target.files[0])}></input>
                             </label>
+                            <button onClick={() => upload()}>Upload</button>
                             <button onClick={() => createPost( id, postTitleRef, userInfo, postTextRef, navigate)} className='bg-blue-500 text-white w-20 py-1 mt-4 rounded-full'>Post</button>
                         </li>
                     </ul>

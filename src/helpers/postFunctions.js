cf0d2e0efb058c06c15b17dc771d6ab9f71039df
Jordan Ayotte <jordanayotte13@outlook.com>
2022-10-25 22:29:31 -0400

    now shows error message when trying to dfelete a post that isnt yours

--- src/helpers/postFunctions.js
+++ src/helpers/postFunctions.js
@@ -1,8 +1,11 @@
 import { doc, deleteDoc } from "firebase/firestore";
 import { db } from '../firebase'
-export async function deletePost(post, user) {
+export async function deletePost(post, user, setError) {
     try {
-        if(user.username !== post.author) return 
+        if(user.username !== post.author) {
+            setError('Only the author of this post can delete or edit it.')
+            return
+        }
         await deleteDoc(doc(db, 'subreddits', post.subredditId, 'posts', post.id))
     }
     catch(e) {
--- src/pages/Comments.js
+++ src/pages/Comments.js
@@ -26,6 +26,7 @@ function Comments(props) {
     const [isUpvotedByUser, setIsUpvotedByUser] = useState(false)
     const [isDownvotedByUser, setIsDownvotedByUser] = useState(false)
     const [authorMenuVisible, setAuthorMenuVisible] = useState(false)
+    const [error, setError] = useState('')
  
     async function postComment() {
         const uniqueId = uniqid()
@@ -171,7 +172,7 @@ function Comments(props) {
                     </svg>
                     {authorMenuVisible ? 
                     <ul className='flex flex-col z-20 gap-4 absolute right-2 top-10 dark:text-white dark:bg-gray-800 px-4 py-4 rounded-md shadow-lg'>
-                        <li className='flex gap-2 cursor-pointer'>
+                        <li className='flex gap-2 cursor-pointer' onClick={() => deletePost(postMetaData, userInfo, setError)}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                             </svg>
@@ -190,6 +191,13 @@ function Comments(props) {
                         <p className='font-bold text-sm cursor-pointer' onClick={() => navigate(`/r/${subredditMetaData.title}`)}>r/{subredditMetaData.title}</p>
                         <p className=' text-xs opacity-50'>u/{postMetaData.author} {getDatefromSeconds(postMetaData.timestamp?.seconds, Timestamp.now().seconds)}</p>
                     </span>
+                    {error && 
+                    <div className='w-full bg-red-700 p-4 font-semibold rounded-md mt-4 text-white flex gap-2'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                        </svg>
+                        {error}
+                    </div>}
                     <h1 className='mt-4 text-2xl break-words'>{postMetaData.postTitle}</h1>
                     <img className='mt-4'  src={postMetaData.imageURL}></img>
                     <p className='mt-4 text-sm break-words'>{postMetaData.text}</p>

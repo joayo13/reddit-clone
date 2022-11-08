e2d63b608f98a7507655d08ab4e9523836c6c35e
Jordan Ayotte <jordanayotte13@outlook.com>
2022-11-08 17:44:40 -0500

    added display of rank of community and green chevron in top communities component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -46,13 +46,21 @@ function Home() {
                       <div className='absolute shadow-[inset_0_-35px_20px_-5px_rgba(0,0,0,0.4)] bottom-0 right-0 top-0 left-0'></div>
                       <h2 className='text-lg font-semibold text-white absolute bottom-2 left-4'>Top Communities</h2>
                     </div>
-                      {topSubreddits.map((subreddit) => <div className='px-4 flex w-full items-center justify-between'><div><p>r/{subreddit.title}</p>
-                      <span className='font-bold text-xs'>{subreddit.joined} Members</span>
-                      </div>
-                      <button 
-                      className='w-16 bg-blue-500 h-6 font-bold rounded-full text-white hover:bg-blue-400 text-sm'
-                      >Join</button>
-                    </div>)}
+                      {topSubreddits.map((subreddit) => 
+                      <div className='px-4 relative w-full'>
+                        <div className='absolute left-4 bottom-1/2 translate-y-1/2 flex items-center gap-2'>
+                        <p className='font-bold text-sm'>{topSubreddits.indexOf(subreddit) + 1}</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="4" stroke="#46d160" class="w-4 h-4">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+                        </svg>
+
+                        </div>
+                        <div className='ml-12'>
+                          <p>r/{subreddit.title}</p>
+                          <span className='font-bold text-xs'>{subreddit.joined} Members</span>
+                        </div>
+                      <button className='absolute right-4 bottom-1/2 translate-y-1/2 w-16 bg-blue-500 h-6 font-bold rounded-full text-white hover:bg-blue-400 text-sm'>Join</button>
+                      </div>)}
                   </li>
                   <li className='flex flex-col px-4 py-4 bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 dark:text-gray-300 gap-4 rounded-md'>
                       <h2 className='text-xl font-semibold'>Popular Communities</h2>

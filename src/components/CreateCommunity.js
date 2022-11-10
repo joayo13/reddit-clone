807a2e413a261e1ca4bd5ea5935d3e2ff726b10e
Jordan Ayotte <jordanayotte13@outlook.com>
2022-11-10 17:10:13 -0500

    added prettier style to the numbered rules in create community component

--- src/components/CreateCommunity.js
+++ src/components/CreateCommunity.js
@@ -97,7 +97,7 @@ function CreateCommunity(props) {
                 {deleteMode ? <svg onClick={() => setSubredditRules(prev => prev.filter((rules) => rules !== rule))} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="#fe3a3a">
                     <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
                 </svg> : null}
-                <li className='font-bold'>{subredditRules.indexOf(rule) + 1}. {rule}</li></div>)}
+                <li className='flex gap-2'><p className='font-bold'>{subredditRules.indexOf(rule) + 1}.</p>{rule}</li></div>)}
             </div>
             </ol>
             <button onClick={() => createCommunity()} className='absolute bottom-4 right-4 rounded-full py-2 px-4 mt-4 bg-blue-600 text-white font-semibold'>Create Community</button>

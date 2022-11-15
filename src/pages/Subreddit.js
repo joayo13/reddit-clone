c047cd169dd1dc27336c69cc0ef67e3ad3adf4a3
Jordan Ayotte <jordanayotte13@outlook.com>
2022-11-15 17:14:22 -0500

    changed style of subreddit rules component

--- src/pages/Subreddit.js
+++ src/pages/Subreddit.js
@@ -54,7 +54,7 @@ function Subreddit (props) {
                         <p className='font-semibold text-sm px-4 pb-4'>{subredditMetaData.joined} Members</p>
                     </li>
                     <li className='flex flex-col bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 dark:text-gray-300 gap-4 rounded-md overflow-hidden'>
-                        <h2 style={{ backgroundColor: subredditMetaData.communityColor }} className='px-4 py-2 text-md text-white font-semibold'>Subreddit Rules</h2>
+                        <h2 style={{ backgroundColor: subredditMetaData.communityColor }} className='px-4 py-2 text-xs text-white font-semibold'>R/{subredditMetaData.title.toUpperCase()} RULES</h2>
                         <ol className='flex flex-col gap-4 list-decimal pb-4'>
                         {subredditMetaData.subredditRules?.map((rule, index) => <li className='px-4 text-sm font-bold' key={index}>{index + 1}. {rule}</li>)}
                         </ol>

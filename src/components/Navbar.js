1467650f604b6205bc15b2c668f9144270ed9290
Jordan Ayotte <jordanayotte13@outlook.com>
2022-12-08 17:29:56 -0500

    squashed bug where 0 notifications would show on page load, now removes info until data is loaded

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -58,9 +58,11 @@ const Navbar = (props) => {
           {currentUser
             ? <span className='hidden md:flex justify-center md:gap-4 items-center'>
             <button className='relative'>
-            <div className='rounded-full flex items-center justify-center z-10 full h-[0.85rem] w-[0.85rem] absolute -top-1 -left-1 bg-[#ff4500]'>
+            {notifications.length > 0
+              ? <div className='rounded-full flex items-center justify-center z-10 full h-[0.85rem] w-[0.85rem] absolute -top-1 -left-1 bg-[#ff4500]'>
               <p className='text-xs font-bold text-white'>{notifications.length}</p>
             </div>
+              : null }
             <svg onClick={() => setNotificationsVisible(true)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6 dark:text-white opacity-50">
             <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
             </svg>
@@ -81,9 +83,11 @@ const Navbar = (props) => {
           </>}
           {currentUser
             ? <button className='relative md:hidden'>
-              <div className='rounded-full flex items-center justify-center z-10 full h-[0.85rem] w-[0.85rem] absolute -top-1 -left-1 bg-[#ff4500]'>
+              {notifications.length > 0
+                ? <div className='rounded-full flex items-center justify-center z-10 full h-[0.85rem] w-[0.85rem] absolute -top-1 -left-1 bg-[#ff4500]'>
               <p className='text-xs font-bold text-white'>{notifications.length}</p>
             </div>
+                : null }
             <svg onClick={() => setNotificationsVisible(true)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className=" md:hidden h-6 w-6 mr-4 dark:text-white opacity-50">
             <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
             </svg>

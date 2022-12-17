507353936a220c3d79e6a7f164d872b5bd6ccb56
Jordan Ayotte <jordanayotte13@outlook.com>
2022-12-17 15:02:03 -0500

    changed spacing values to be consistent in comments component in post info

--- src/pages/Comments.js
+++ src/pages/Comments.js
@@ -200,9 +200,9 @@ function Comments (props) {
                         </svg>
                         {error}
                     </div>}
-                    <h1 className='mt-4 text-2xl break-words'>{postMetaData.postTitle}</h1>
-                    <img className='mt-4 z-10' src={postMetaData.imageURL}></img>
-                    <p className='mt-4 text-sm break-words'>{postMetaData.text}</p>
+                    <h1 className='py-2 text-2xl break-words'>{postMetaData.postTitle}</h1>
+                    <img className=' z-10' src={postMetaData.imageURL}></img>
+                    <p className='py-2 text-sm break-words'>{postMetaData.text}</p>
                     <li className='flex gap-2 opacity-50 mt-4'>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 opacity-50" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />

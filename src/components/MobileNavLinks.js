56f945fda217d26740bebf5e0b08870687f9983b
Jordan Ayotte <jordanayotte13@outlook.com>
2022-12-18 23:36:06 -0500

    removed help center and terms and policeis in mobilenavlinks component

--- src/components/MobileNavLinks.js
+++ src/components/MobileNavLinks.js
@@ -27,18 +27,6 @@ function MobileNavLinks (props) {
       </svg>
       Dark Mode
     </button>
-    <li className='flex gap-2 items-center cursor-pointer px-2 py-4 hover:bg-gray-300 dark:hover:bg-gray-800'>
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-      </svg>
-      Help Center
-    </li>
-    <li className='flex gap-2 items-center cursor-pointer px-2 py-4 hover:bg-gray-300 dark:hover:bg-gray-800'>
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
-      </svg>
-      {'Terms & Policies'}
-    </li>
     <div className='w-auto h-px bg-gray-300 dark:bg-gray-700'></div>
     <> {!currentUser
       ? <li className='flex gap-2 items-center cursor-pointer px-2 py-4 hover:bg-gray-300 dark:hover:bg-gray-800' onClick={() => { props.setMobileNavLinksVisible(false); props.setLogInPopUp(true) }}>

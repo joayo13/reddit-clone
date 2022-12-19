7581040221d1b6cb74387b22eb0a069218be2ec0
Jordan Ayotte <jordanayotte13@outlook.com>
2022-12-19 13:17:23 -0500

    added styles to remove picture button in submit post component

--- src/pages/SubmitPost.js
+++ src/pages/SubmitPost.js
@@ -32,7 +32,11 @@ function SubmitPost () {
                         <li className= 'py-2'>
                             <div className='relative'>
                               {imageURL
-                                ? <button onClick={() => deleteImage(fileSelected, setImageURL)} className='absolute top-0 right-0 bg-red-500'>Delete</button>
+                                ? <button onClick={() => deleteImage(fileSelected, setImageURL)} className='absolute -top-2 -right-2'>
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="grey" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" className="w-12 h-12">
+                                  <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                  </svg>
+                                </button>
                                 : null }
                             <img src={imageURL} className='pb-4'></img>
                             </div>

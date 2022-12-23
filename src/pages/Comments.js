85656988ba7efc5010e2cfb90d6f59f9ecb8568e
Jordan Ayotte <jordanayotte13@outlook.com>
2022-12-23 18:22:39 -0500

    fixed delete message when poist is deleted to show properly

--- src/pages/Comments.js
+++ src/pages/Comments.js
@@ -204,7 +204,7 @@ function Comments (props) {
                     </div>}
                     <h1 className='py-2 text-2xl break-words'>{postMetaData.postTitle}</h1>
                     <img className='z-10' src={postMetaData.imageURL}></img>
-                    {!postMetaData.title ? <p>[deleted]</p> : null}
+                    {!postMetaData.postTitle ? <p>[deleted]</p> : null}
                     {!editMode
                       ? <p className='py-2 text-sm break-words'>{postMetaData.text}</p>
                       : <div>

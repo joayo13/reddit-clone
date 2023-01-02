f3ce2c171e6d669955c46ec4fa56fe27b5f9e17a
Jordan Ayotte <jordanayotte13@outlook.com>
2023-01-01 19:39:56 -0500

    input on createpostcard component no longer disabled

--- src/components/CreatePostCard.js
+++ src/components/CreatePostCard.js
@@ -9,7 +9,7 @@ function CreatePostCard (props) {
     <div>
         <button onClick={() => navigate(`/r/${id}/submit`)} className='flex gap-2 px-4 py-4 w-full bg-white dark:bg-neutral-900 dark:border-neutral-800 border border-neutral-300 md:rounded-md'>
             <img src={userInfo.profilePicture} className='w-10 rounded-full'></img>
-             <input onClick={() => navigate(`/r/${id}/submit`)} disabled={true} type='text' placeholder='Create Post' className='w-full outline-none bg-neutral-100 dark:bg-neutral-800 dark:text-white indent-2 rounded-md dark:border-neutral-700 border border-neutral-200'></input>
+             <input onClick={() => navigate(`/r/${id}/submit`)} type='text' placeholder='Create Post' className='w-full cursor-text outline-none bg-neutral-100 dark:bg-neutral-800 dark:text-white indent-2 rounded-md dark:border-neutral-700 border border-neutral-200'></input>
         </button>
     </div>
   )
